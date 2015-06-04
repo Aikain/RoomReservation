c13fb65aa4be73ad2569d9cd9ce7f6ef48270376
Aikain <aikain@paivola.fi>
2015-06-04 13:13:57 +0300

    Debug roomNro

--- src/main/java/fi/gosu/roomreservation/domain/Room.java
+++ src/main/java/fi/gosu/roomreservation/domain/Room.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -12,6 +14,7 @@ import javax.persistence.OneToMany;
 public class Room implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long roomNro;
     @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private List<Reservation> reservations;
