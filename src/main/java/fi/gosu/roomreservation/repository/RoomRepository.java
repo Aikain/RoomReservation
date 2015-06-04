4cd46a0a3071b908161e38003d9075fd1e9e3324
Aikain <aikain@paivola.fi>
2015-06-04 13:32:55 +0300

    Added orderby to room repository

--- src/main/java/fi/gosu/roomreservation/repository/RoomRepository.java
+++ src/main/java/fi/gosu/roomreservation/repository/RoomRepository.java
@@ -1,8 +1,10 @@
 package fi.gosu.roomreservation.repository;
 
 import fi.gosu.roomreservation.domain.Room;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
+    public List<Room> findAllOrderByRoomNro();
 }
