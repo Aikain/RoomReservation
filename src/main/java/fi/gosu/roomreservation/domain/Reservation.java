0a21c2054f82254e3c04cb306885dc66867402bc
Aikain <aikain@paivola.fi>
2015-06-04 20:59:30 +0300

    testing multiple person v2

--- src/main/java/fi/gosu/roomreservation/domain/Reservation.java
+++ src/main/java/fi/gosu/roomreservation/domain/Reservation.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
@@ -21,6 +22,7 @@ public class Reservation implements Serializable {
     private Date startTime;
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private Date endTime;
+    @OneToMany
     private List<String> persons;
     @ManyToOne
     private Room room;
