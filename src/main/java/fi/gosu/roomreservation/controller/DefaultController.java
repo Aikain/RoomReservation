b66486f543cc0f26a68ebdbc934928b9d4a5f0a2
Aikain <aikain@paivola.fi>
2015-06-04 13:40:34 +0300

    testing sorting..

--- src/main/java/fi/gosu/roomreservation/controller/DefaultController.java
+++ src/main/java/fi/gosu/roomreservation/controller/DefaultController.java
@@ -2,6 +2,7 @@ package fi.gosu.roomreservation.controller;
 
 import fi.gosu.roomreservation.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,10 @@ public class DefaultController {
 
     @Autowired
     private RoomRepository roomRepository;
-    
+
     @RequestMapping(method = RequestMethod.GET)
     public String view(Model model) {
-        model.addAttribute("rooms", roomRepository.findAllOrderByRoomNroAsc());
+        model.addAttribute("rooms", roomRepository.findAll(new Sort(Sort.Direction.ASC, "roomNro")));
         return "index";
     }
 
--- src/main/java/fi/gosu/roomreservation/repository/RoomRepository.java
+++ src/main/java/fi/gosu/roomreservation/repository/RoomRepository.java
@@ -1,10 +1,8 @@
 package fi.gosu.roomreservation.repository;
 
 import fi.gosu.roomreservation.domain.Room;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-    public List<Room> findAllOrderByRoomNroAsc();
 }
